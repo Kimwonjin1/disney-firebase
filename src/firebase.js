558777980f18d2189624681de0e4411dbf8c5257
kimwonjin1 <dnjswls1459@gmail.com>
2022-07-22 11:11:06 +0900

    disney

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGrdvVhy0dfchLwvF1NoExsA_GmnQijcY",
+  authDomain: "disney-ddd33.firebaseapp.com",
+  projectId: "disney-ddd33",
+  storageBucket: "disney-ddd33.appspot.com",
+  messagingSenderId: "647594861383",
+  appId: "1:647594861383:web:6825eb46a711a283482262",
+  measurementId: "G-JMD8K7Q3D4"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
